7d5a3d599acd784f8be8502b6a2df32e31b256cf
edwin <edwincamilo@live.com>
2024-07-31 17:50:00 -0500

    miercoles trabajo

--- src/main.js
+++ src/main.js
@@ -6,13 +6,19 @@ document.querySelectorAll('h2').forEach((element) => {
     element.classList.add('subtitulos');
 });
 
+document.querySelectorAll('h3').forEach((element) => {
+    element.classList.add('titulos-blog');
+});
+
+document.querySelectorAll('h4').forEach((element) => {
+    element.classList.add('introduce');
+});
+
+
 document.querySelectorAll('p').forEach((element) => {
     element.classList.add('parrafo');
 });
 
-document.querySelectorAll('h3').forEach((element) => {
-    element.classList.add('titulos-blog');
-});
 
 
 
